6e8eb74fef3d4cb3e88957c7f1e72e23777acd8c
Vishal Garud <vishal.g@ezdi.us>
2016-02-04 18:34:16 +0530

    change 1st

--- GitTraining/src/main/java/com/ezdi/client/Test.java
+++ GitTraining/src/main/java/com/ezdi/client/Test.java
@@ -6,17 +6,17 @@ import java.util.Map;
 public class Test {
 
 	public static void main(String[] args) {
-	
-		Map<String,String> codeMap= new HashMap<String, String>();
+
+		Map<String, String> codeMap = new HashMap<String, String>();
 		codeMap.put("A00.0", "Vibrio cholerae 01<OR>biovar cholerae");
 		codeMap.put("A01", "Typhoid<AND>paratyphoid fevers");
 		codeMap.put("A15.5", "Tuberculosis<AND>(larynx<OR>trachea<AND>bronchus)");
 		codeMap.put("A06.4", "Amebic liver abscess");
 		codeMap.put("A01.01", "Typhoid meningitis");
 		codeMap.put("A01.02", "Typhoid fever<AND>heart involvement");
-		codeMap.put("A01.03","Abdominal  aortic <AND> aneurysm");
-		
-		String query="Abdominal  aortic <AND> aneurysm";
+		codeMap.put("A01.03", "Abdominal  aortic <AND> aneurysm");
+
+		String query = "Abdominal  aortic <AND> aneurysm";
 
 		String value = codeMap.get("A01.02");
 		if (value != null) {
